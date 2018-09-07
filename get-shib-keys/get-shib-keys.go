5420b4f207c0b7f5423099c99a4c2598e8fe212d
Yoon Lee <yoonlees@uiuc.edu>
2018-09-07 10:00:53 -0500

    getParamKey function works

--- get-shib-keys/get-shib-keys.go
+++ get-shib-keys/get-shib-keys.go
@@ -32,78 +32,81 @@ const NoSecret = 3
 // FileError - File system error
 const FileError = 4
 
-func getParamKey(svc *ssm.SystemsManager, keyname string) (string, error) {
+func getParamKey(svc *ssm.SSM, keyname string, decription bool) (string, error) {
 
-    input := &ssm.GetParameterInput{
-            Name: aws.String(keyname),
-            WithDecryption: aws.Bool(decription)
-    }
+	input := &ssm.GetParameterInput{
+		Name:           aws.String(keyname),
+		WithDecryption: aws.Bool(decription),
+	}
 
-    result, err := svc.GetParameter(input)
+	result, err := svc.GetParameter(input)
 
-    if err != nil {
-        fmt.Fprint(os.Stderr, "Failed to find param for keyname: ", keyname, "\n\n\a")
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Failed to find param for keyname: ", keyname, "\n\n\a")
 
-        if aerr, ok := err.(awserr.Error); ok {
-                switch aerr.Code() {
-                case ssm.ErrCodeInternalServerError:
-                        fmt.Fprintln(os.Stderr, ssm.ErrCodeInternalServerError, aerr.Error())
-                case ssm.ErrCodeInvalidKeyId:
-                        fmt.Fprintln(os.Stderr, ssm.ErrCodeInvalidKeyId, aerr.Error())
-                case ssm.ErrCodeParameterNotFound:
-                        fmt.Fprintln(os.Stderr, ssm.ErrCodeParameterNotFound, aerr.Error())
-                case ssm.ErrCodeParameterVersionNotFound:
-                        fmt.Fprintln(os.Stderr, ssm.ErrCodeParameterVersionNotFound, aerr.Error())
-                default:
-                        fmt.Fprintln(os.Stderr, aerr.Error())
-                }
-        } else {
-                // Print the error, cast err to awserr.Error to get the Code and
-                // Message from an error.
-                fmt.Fprintln(os.Stderr, err.Error())
-        }
-        return "", err
-    }
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ssm.ErrCodeInternalServerError:
+				fmt.Fprintln(os.Stderr, ssm.ErrCodeInternalServerError, aerr.Error())
+			case ssm.ErrCodeInvalidKeyId:
+				fmt.Fprintln(os.Stderr, ssm.ErrCodeInvalidKeyId, aerr.Error())
+			case ssm.ErrCodeParameterNotFound:
+				fmt.Fprintln(os.Stderr, ssm.ErrCodeParameterNotFound, aerr.Error())
+			case ssm.ErrCodeParameterVersionNotFound:
+				fmt.Fprintln(os.Stderr, ssm.ErrCodeParameterVersionNotFound, aerr.Error())
+			default:
+				fmt.Fprintln(os.Stderr, aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
+		return "", err
+	}
 
-    return *result.Parameter.Value, nil
+	return *result.Parameter.Value, nil
 }
 
 func args() (string, string) {
-    filePtr := flag.StringP("file", "f", "", "a file used to store the shib keys.")
-    flag.Usage = func() {
-            fmt.Printf("Usage: get-shib-keys [options] keyname\n\n")
-            flag.PrintDefaults()
-    }
+	filePtr := flag.StringP("file", "f", "", "a file used to store the shib keys.")
+	flag.Usage = func() {
+		fmt.Printf("Usage: get-shib-keys [options] keyname\n\n")
+		flag.PrintDefaults()
+	}
 
-    flag.Parse()
-    if len(flag.Args()) != 1 {
-            flag.Usage()
-            os.Exit(BadArgs)
-    }
+	flag.Parse()
+	if len(flag.Args()) != 1 {
+		flag.Usage()
+		os.Exit(BadArgs)
+	}
 
-    return *filePtr, flag.Args()[0]
+	return *filePtr, flag.Args()[0]
 }
 
 func main() {
-    filename, keyname := args()
+	filename, keyname := args()
 
-    sess := session.Must(session.NewSessionWithOptions(session.Options{
-            SharedConfigState: session.SharedConfigEnable,
-    }))
-    svc := ssm.New(sess)
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	svc := ssm.New(sess)
 
-    param, err := getParamKey(svc, keyname, false)
+	param, err := getParamKey(svc, keyname, false)
+	if err != nil {
+		os.Exit(1)
+	}
 
-    if filename == "" {
-            fmt.Print(param)
-    } else {
-            err := ioutil.WriteFile(filename, []byte(param), 0600)
+	if filename == "" {
+		fmt.Print(param)
+	} else {
+		err := ioutil.WriteFile(filename, []byte(param), 0600)
 
-            if err != nil {
-                    fmt.Fprintln(os.Stderr, err)
-                    os.Exit(FileError)
-            }
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(FileError)
+		}
 
-            fmt.Fprint(os.Stderr, "Wrote shib key to file: '", filename, "'.  Retrieved from AWS Parameter: '", keyname, ".'\n")
-    }
+		fmt.Fprint(os.Stderr, "Wrote shib key to file: '", filename, "'.  Retrieved from AWS Parameter: '", keyname, ".'\n")
+	}
 }
