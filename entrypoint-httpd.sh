2fbfb5874e494bd9708c6d8c92b3c66482ca412c
David D. Riddle <ddriddle@illinois.edu>
2018-09-14 07:06:52 -0500

    Fixup

--- entrypoint-httpd.sh
+++ entrypoint-httpd.sh
@@ -18,10 +18,6 @@
 #
 # ENVIRONMENT
 #
-#   APP_SERVER_NAME
-#           Name of the application server (the host name or 
-#           virtual host name). Set in docker-compose.yml.
-#
 #   SHIBD_HOSTNAME
 #           Name of the container (host) running shibd. Set in 
 #           docker-compose.yml. If not set then assume 127.0.0.1.
@@ -31,9 +27,6 @@
 set -e 
 
 TIME=0.1
-
-# Make sure user set necessary environment variables
-[ -z "$APP_SERVER_NAME" ] && echo "APP_SERVER_NAME is not set!" && exit 3
 
 if [ -z "$SHIBD_HOSTNAME" ]; then
     # Assuming we are running awsvpc mode
@@ -49,11 +42,6 @@ else
     echo "Shibboleth IP is $SHIBD_IP."
 fi
 
-sed -i -e "s/SHIBD_IP/$SHIBD_IP/g" \
-    -e "s/APP_SERVER_NAME/$APP_SERVER_NAME/g" \
-    -e "s/SHIBD_IDP/$SHIBD_IDP/g" /etc/shibboleth/shibboleth2.xml
-
-sed -i -e "s/APP_SERVER_NAME/$APP_SERVER_NAME/g" \
-    /etc/httpd/conf/httpd.conf
+sed -i -e "s/SHIBD_IP/$SHIBD_IP/g" /etc/shibboleth/shibboleth2.xml
 
 exec httpd -DFOREGROUND
