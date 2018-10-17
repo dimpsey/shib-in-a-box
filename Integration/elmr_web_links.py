88026e99525a8039130171de20cb33e496934ad5
Kala Maturi <cmaturi@illinois.edu>
2018-10-17 16:20:51 -0500

    Revert "Behave feature file and step files to test Elmr Sample Attributes"
    This reverts commit 18ac7eddba43d68e0e196cc4e1abc6b28220743c.

--- Integration/elmr_web_links.py
+++
@@ -1,9 +0,0 @@
-'''
-collection of all URLs used in the tests.
-
-'''
-
-LOGIN = 'https://multi-service.as-test.techservices.illinois.edu/elmrsample/attributesi'
-UIUC_PARTIAL_URL = 'https://shibboleth.illinois.edu/idp/profile/SAML2/Redirect/SSO?execution=e2s1'
-ELMR_URL = 'https://multi-service.as-test.techservices.illinois.edu/elmrsample/attributes'
-LOGOUT_URL = 'https://shibboleth.illinois.edu/idp/profile/Logout?execution=e2s1'
