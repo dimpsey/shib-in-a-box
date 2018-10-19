ba7a340b0094ffd93ec52799c0684ccf92d2b795
David D. Riddle <ddriddle@illinois.edu>
2018-10-19 13:08:58 -0500

    Refactor http-status
    - Remove logging code
    - Now can check multiple urls

--- http-status/http-status.go
+++ http-status/http-status.go
@@ -1,74 +1,130 @@
+// http-status is used to check the http status code returned by a website.
+// This is command is intended to be used as Docker health check for web
+// services.
+//
 // https://gobyexample.com/ is very helpful if you do not know Go.
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"net/http/httputil"
 	"os"
+	"strconv"
 )
 
 // GNU style POSIX standard alternative to flag
 // https://godoc.org/github.com/spf13/pflag
 import flag "github.com/spf13/pflag"
 
-// better logger
-import "go.uber.org/zap"
+// Healthy - returned status code is good
+const Healthy = 0
 
-var SLogger *zap.SugaredLogger
+// Unhealthy - returned status code is bad
+const Unhealthy = 1
 
-// Success - returned status code is good
-const Success = 0
-
-// Fail - returned status code is bad
-const BadResponse = 1
-
-// BadArgs - Bad arguments passed
-const BadArgs = 2
-
-// HttpError - Http call error
-const HttpError = 3
-
-// GoodResonseCode - Expected respose code
-const GoodResponse = 202
-
-func InitSugarLogger() {
-	logger, _ := zap.NewDevelopment()
-	defer logger.Sync() // flushes buffer, if any
-
-	SLogger = logger.Sugar()
+type Args struct {
+	body  bool
+	get   bool
+	quiet bool
 }
 
-func args() (string, string) {
-	urlPtr := flag.StringP("url", "c", "", "URL to test the status")
+func args() ([]string, []string, Args) {
+	get := flag.BoolP("get", "g", false,
+		"Use GET instead of HEAD")
+	body := flag.BoolP("body", "b", false,
+		"Print full body of response. By default only headers are shown")
+	quiet := flag.BoolP("quiet", "q", false,
+		"Quiet mode (don't output anything)")
+	statusCodes := flag.StringArrayP("expected-status-code", "c", []string{"200"},
+		"Check expected status code is returned.")
+
 	flag.Usage = func() {
-		SLogger.Info("Usage: http-status [options] url\n\n")
+		fmt.Fprintln(os.Stderr, "Usage: http-status URL1 URL2 ... URLN")
 		flag.PrintDefaults()
 	}
 
 	flag.Parse()
-	if len(flag.Args()) != 1 {
+	if len(flag.Args()) == 0 {
 		flag.Usage()
-		os.Exit(BadArgs)
+		os.Exit(1)
 	}
 
-	return *urlPtr, flag.Args()[0]
+	if len(*statusCodes) > 1 {
+		if len(*statusCodes) > len(flag.Args()) {
+			fmt.Println("Too many status codes given!\a")
+			os.Exit(1)
+		} else if len(*statusCodes) < len(flag.Args()) {
+			fmt.Println("Not enough status codes given!\a")
+			os.Exit(1)
+		}
+	}
+
+	if *body {
+		*get = true
+	}
+
+	return *statusCodes, flag.Args(), Args{*body, *get, *quiet}
+}
+
+func atoi(s string) int {
+	n, err := strconv.Atoi(s)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s is out of range!\n\a", s)
+		os.Exit(1)
+	}
+
+	return n
 }
 
 func main() {
-	InitSugarLogger()
+	codes, urls, arguments := args()
 
-	_, url := args()
+	code := atoi(codes[0])
+	for i, url := range urls {
+		if i > 0 && len(codes) > 1 {
+			code = atoi(codes[i])
+			fmt.Println("")
+		}
 
-	resp, err := http.Head(url)
-	if err != nil {
-		SLogger.Error("Something went wrong with http call")
-		SLogger.Error(err.Error())
-		os.Exit(HttpError)
+		healthcheck(code, url, arguments)
 	}
 
+	os.Exit(0)
+}
+
+func healthcheck(code int, url string, args Args) {
+	var err error
+	var resp *http.Response
+
+	if args.get {
+		resp, err = http.Get(url)
+	} else {
+		resp, err = http.Head(url)
+	}
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(Unhealthy)
+	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != GoodResponse {
-		SLogger.Infof("Bad Response code: %d", resp.StatusCode)
-		os.Exit(BadResponse)
+	dump, err := httputil.DumpResponse(resp, args.body)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(Unhealthy)
+	}
+
+	if !args.quiet {
+		fmt.Printf("%s", dump)
+	}
+
+	if resp.StatusCode != code {
+		if !args.quiet {
+			fmt.Printf("Bad Response code: %d\n", resp.StatusCode)
+			fmt.Printf("Expected Response code: %d\n", code)
+		}
+		os.Exit(Unhealthy)
 	}
 }
