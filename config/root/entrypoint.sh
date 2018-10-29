ae79832edb11cf61d080c7b21603bc8007c5b372
David D. Riddle <ddriddle@illinois.edu>
2018-10-29 14:24:22 -0500

    shibd seems to work!
    Now just need to finish httpd!

---
+++ config/root/entrypoint.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+set -e
+
+function get_ip_addr() {
+    # $1 is var to set with IP
+    # $2 is hostname to lookup
+    TIME=0.1
+
+    if [ -z "$2" ]; then
+        # Assuming we are running in awsvpc mode
+        IP="127.0.0.1"
+    else
+        # Assuming we are running in Bridge mode or docker-compose
+        echo "Waiting for IP to become avaliable from HOST '$2'..."
+        while [ -z "$IP" ]; do
+            IP=$(getent ahosts $2 | awk 'NR==1{ print $1 }')
+            echo "    Sleeping for $TIME seconds."
+            sleep $TIME
+        done
+        echo "HOST $2 has the IP: $IP."
+    fi
+
+    eval export $1=$IP
+}
+
+get_ip_addr HTTPD_IP "$HTTPD_HOSTNAME"
+# get_ip_addr SHIBD_IP "$SHIBD_HOSTNAME"
+
+# TODO '/service/shibd' needs to be configurable by ENV VAR!
+/usr/local/bin/get-shib-keys /service/shibd 
+
+sed -i -e "s/SHIBD_ACL/$HTTPD_IP/g" $SHIBSP_CONFIG
+# sed -i -e "s/SHIBD_IP/$SHIBD_IP/g"  $SHIBSP_CONFIG
+
+################################################################
+#set -e 
+#
+#TIME=0.1
+#HTTPD_CONF=/etc/httpd/conf/httpd.conf
+#
+#if [ -z "$ELMR_HOSTNAME" ]; then
+#    # Assuming we are running awsvpc mode
+#    export ELMR_IP="127.0.0.1"
+#else
+#    # Assuming we are running Bridge mode or docker-compose
+#    echo "Waiting for elmr IP to become avaliable..."
+#    while [ -z "$ELMR_IP" ]; do
+#        export ELMR_IP=$(getent ahosts $ELMR_HOSTNAME | awk 'NR==1{ print $1 }')
+#        echo "    Sleeping for $TIME seconds."
+#        sleep $TIME
+#    done
+#    echo "elmr IP is $ELMR_IP."
+#fi
+#
+#if [ -z "$LB_HOSTNAME" ]; then
+#    sed -i -e "/LB_HOSTNAME/d" $HTTPD_CONF
+#else
+#    sed -i -e "s/LB_HOSTNAME/$LB_HOSTNAME/g" $HTTPD_CONF
+#fi
+#
+#sed -i -e "s/SHIBD_IP/$SHIBD_IP/g" $SHIBSP_CONFIG
+#
+#exec httpd -DFOREGROUND
--- shibd/entrypoint.sh
+++ shibd/entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 
 TIME=0.1
 KEYS=/var/shib-keys/keys
-CONFIG=/var/run/shibboleth/shibboleth2.xml
 
 echo "Waiting for $KEYS..."
 while [ ! -s "$KEYS" ]; do
@@ -13,4 +12,4 @@ while [ ! -s "$KEYS" ]; do
 done
 echo "Found $KEYS!"
 
-exec /usr/sbin/shibd -F -f -c $CONFIG
+exec /usr/sbin/shibd -F -f
