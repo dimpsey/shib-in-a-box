24adc589e8f409f4765592e6f22cf3257e2631a8
Yoon Lee <yoonlees@illinois.edu>
2018-11-16 11:24:42 -0600

    Fix elmr config not using shib

--- httpd/root/entrypoint.sh
+++ httpd/root/entrypoint.sh
@@ -8,6 +8,9 @@ TIME=0.1
 [[ -z "$LB_HOSTNAME" ]] && echo "LB_HOSTNAME not set!" && exit 1
 [[ -z "$LOG_LEVEL" ]] && echo "LOG_LEVEL not set!" && exit 1
 
+# By default, disable elmr_config
+[[ -z "$ENABLE_ELMR_CONFIG" ]] && rm /etc/httpd/conf.d/elmr_config.conf
+
 echo "Waiting for $SHIBSP_CONFIG..."
 while [ ! -s "$SHIBSP_CONFIG" ]; do
     echo "    Sleeping for $TIME seconds."
