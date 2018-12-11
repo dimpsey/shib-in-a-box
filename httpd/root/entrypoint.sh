5b3a4d2e2932d0a6718983cd992809e8e62e86ee
David D. Riddle <ddriddle@illinois.edu>
2018-12-11 17:04:53 -0600

    Remove mock_shib.conf if not used

--- httpd/root/entrypoint.sh
+++ httpd/root/entrypoint.sh
@@ -8,9 +8,10 @@ TIME=0.1
 [[ -z "$LB_HOSTNAME" ]] && echo "LB_HOSTNAME not set!" && exit 1
 [[ -z "$LOG_LEVEL" ]] && echo "LOG_LEVEL not set!" && exit 1
 
-# By default, disable elmr_config
+# By default, delete config files used for testing
 [[ -z "$ENABLE_ELMR_CONFIG" ]] && rm /etc/httpd/conf.d/elmr_config.conf
 [[ -z "$ENABLE_ENVIRONMENT_PAGE" ]] && rm /etc/httpd/conf.d/environment.conf
+[[ -z "$NO_SHIBD" ]] && rm /etc/httpd/conf.d/mock_shib.conf
 
 echo "Waiting for $SHIBSP_CONFIG..."
 while [ ! -s "$SHIBSP_CONFIG" ]; do
