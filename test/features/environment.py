ab6c6eda672a2937097ddb56ea20f7deb20d7bc8
Yoon Lee <yoonlees@illinois.edu>
2018-12-12 12:10:28 -0600

    Add basic test

--- test/features/environment.py
+++ test/features/environment.py
@@ -4,7 +4,12 @@ testing our Terraform modules.
 .../as-aws-module/test/features/environment.py
 """
 import os
+import pprint
+import subprocess
 import sys
+import time
+
+from configparser import ConfigParser
 from sdg.test.behave import (
     core
 )
@@ -13,8 +18,8 @@ MODULES = [
 ]
 
 DIR = os.path.abspath(os.path.dirname(__file__))
-PROJECT_DIR = os.path.abspath(os.path.join(DIR, "../../"))
-CONF_DIR = os.path.normpath(os.path.join(DIR, "../config"))
+PROJECT_DIR = os.path.abspath(os.path.join(DIR, "../../../"))
+CONF_DIR = os.path.normpath(os.path.join(DIR, "../../config"))
 
 core.SET_config(path=os.path.join(CONF_DIR, "core.conf")) #Override default core conf values.
 
@@ -24,4 +29,35 @@ def before_all(context):
     core.SET_REQ_VARS(context,
         project_root=PROJECT_DIR,
         test_root=DIR)
+    
+    # TODO: Figure out way to see if aws is availble
+    p = subprocess.Popen(['docker-compose', 'up', '-d'], 
+        cwd=PROJECT_DIR, env=get_environment())
+    p.wait()
+    time.sleep(5)
 
+
+def after_all(context):
+    """Clean up environment"""
+    p = subprocess.Popen(['docker-compose', 'down'], cwd=PROJECT_DIR)
+    p.wait()
+
+
+def get_environment():
+    ''' Get environement to use '''
+
+    env = os.environ.copy()
+
+    config = ConfigParser()
+    config.optionxform = str
+    config.read(os.path.join(DIR, 'config.ini'))
+
+    return env.update(config['Environment'])
+ 
+
+def main():
+    pass
+
+if __name__ == '__main__':
+  main()
+    
