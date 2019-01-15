1bd12dc6cc6fc352a6d04e35499996b9977ed1e7
Yoon Lee <yoonlees@illinois.edu>
2019-01-15 11:24:40 -0600

    Redis page works

--- httpd/redis.go
+++ httpd/redis.go
@@ -3,76 +3,16 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
 	"os"
-	"strings"
 )
 
 import "github.com/go-redis/redis"
-
-var (
-	Trace   *log.Logger
-	Info    *log.Logger
-	Warning *log.Logger
-	Error   *log.Logger
-)
-
-// Return codes
-
-// NoEnvVar - Environment variable empty or missing
-const NoEnvVar = 1
-
-// NoRedisKey - Unable to find the redis key
-const NoRedisKey = 2
-
-// Init for logging
-func InitLoggers(logLevel string) {
-
-	var traceHandle io.Writer = os.Stderr
-	var infoHandle io.Writer = os.Stderr
-	var warningHandle io.Writer = os.Stderr
-	var errorHandle io.Writer = os.Stderr
-
-	switch strings.ToLower(logLevel) {
-	case "trace":
-		break
-	case "error":
-		warningHandle = ioutil.Discard
-		fallthrough
-	case "warning", "warn":
-		infoHandle = ioutil.Discard
-		fallthrough
-	default:
-		// default log level is info
-		traceHandle = ioutil.Discard
-	}
-
-	Trace = log.New(traceHandle,
-		"TRACE: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	Info = log.New(infoHandle,
-		"INFO: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	Warning = log.New(warningHandle,
-		"WARNING: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	Error = log.New(errorHandle,
-		"ERROR: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-}
 
 func getEnv(key string) string {
 	value := os.Getenv(key)
 
 	if value == "" {
-		fmt.Println("{\"error\": \"Environment variable is undef: ", key, "\"}")
-		//   os.Exit(NoEnvVar)
-		os.Exit(0)
+		PrintError("Environment variable is undef: "+key, nil)
 	}
 	return value
 }
@@ -87,43 +27,37 @@ func RedisNewClient() *redis.Client {
 		DB:       0,  // use default DB
 	})
 
-	// pong, err := client.Ping().Result()
-	// fmt.Println(pong, err)
-
-	// Output: PONG <nil>
 	return client
 }
 
 // Retrieve the value for the Elmr session key
-func GetSessionValue(sessionKey string) (string, error) {
+func GetSessionValue(sessionKey string) string {
 
 	client := RedisNewClient()
 
 	val, err := client.Get(sessionKey).Result()
 
 	if err != nil {
-		Error.Println("No Redis value with key", sessionKey, "found!")
-		return "", err
+		PrintError("Redis key not found:"+sessionKey, err)
 	}
 
-	return val, nil
+	return val
+}
+
+func PrintError(errMsg string, err error) {
+	fmt.Println("Status: 400 Bad Request\n")
+	if err != nil {
+		fmt.Println("{\"error\": \"" + errMsg + "\", \"redisError\": \"" + err.Error() + "\"}")
+	} else {
+		fmt.Println("{\"error\": \"" + errMsg + "\"}")
+	}
+	os.Exit(0)
 }
 
 func main() {
-	// InitLoggers(os.Getenv("LOG_LEVEL"))
+	fmt.Println("content-type: application/json; charset=utf-8")
+	value := GetSessionValue(getEnv("AJP_Shib_Session_ID"))
 
-	// fmt.Println("content-type: application/json; charset=utf-8\n")
-	// value, err := GetSessionValue(getEnv("AJP_Shib_Session_ID"))
-	/*
-		if err != nil {
-	                fmt.Println("{\"error\": \"No Redis key is found\"}")
-			// os.Exit(NoRedisKey)
-			os.Exit(0)
-		}
-
-		fmt.Println(value)
-	*/
-
-	fmt.Println("Content-type: text/plain\n")
-	fmt.Println("{\"a\":\"b\"}")
+	fmt.Println("\n")
+	fmt.Println(value)
 }
